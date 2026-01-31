a17cccbc48d04976bcf36536b15ba11f0ca385b6
Seth Kerrey <sethkerrey@gmail.com>
2026-01-31 10:43:51 -0600

    update vercel config

--- admin/vite.config.ts
+++ admin/vite.config.ts
@@ -2,8 +2,21 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 
 export default defineConfig(() => {
+  const vercelDevUrl = process.env.VERCEL_DEV_URL
+  const proxy =
+    vercelDevUrl != null && vercelDevUrl !== ""
+      ? {
+          "/content": { target: vercelDevUrl, changeOrigin: true },
+          "/api": { target: vercelDevUrl, changeOrigin: true }
+        }
+      : undefined
+
   return {
     base: "/admin",
-    plugins: [react()]
+    plugins: [react()],
+    server: {
+      proxy,
+      port: 5173
+    }
   }
 })
