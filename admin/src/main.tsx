21f49d577d11f19ad6b9592f425f1fa4e8ce9cfa
Seth Kerrey <sethkerrey@gmail.com>
2026-02-01 18:54:43 -0600

    add routing

--- admin/src/main.tsx
+++ admin/src/main.tsx
@@ -1,10 +1,19 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import App from "./App"
+import DownloadPage from "./pages/DownloadPage"
+import NotFound from "./pages/NotFound"
 import "./styles.css"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/download" element={<DownloadPage />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>
 )
